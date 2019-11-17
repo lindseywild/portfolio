152f0725eecd0a427c1fd6e34cbfe4b591a3e138
Lindsey Wild <35239154+lindseywild@users.noreply.github.com>
2019-11-17 09:37:29 -0500

    chore: removes gitignored files (#2)

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#1D201F","theme_color":"#C58882","display":"minimal-ui","icon":"src/images/gatsby-icon.png"},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
