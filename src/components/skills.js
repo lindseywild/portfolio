e6b1cde24c2ff3c0e837b7df8421661e463fe384
lindseywild <lstanek4@gmail.com>
2020-06-24 17:31:45 -0400

    feat: updates t&n experience, some grammar/reordering

--- src/components/skills.js
+++ src/components/skills.js
@@ -7,12 +7,12 @@ const Skills = () => (
       <li>HTML</li>
       <li>Sass/CSS</li>
       <li>JavaScript (ES6)</li>
-      <li>React</li>
-      <li>Unit Testing (Sass, JavaScript, React)</li>
       <li>Accessibility</li>
+      <li>React</li>
+      <li>Testing (React Testing Library, Jest, Enzyme, Cypress)</li>
       <li>Design Systems</li>
+      <li>Conference Speaking/Internal Trainigs</li>
       <li>Dev Team Management</li>
-      <li>Conference Speaking</li>
     </ul>
   </div>
 )
